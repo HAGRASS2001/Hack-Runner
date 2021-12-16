0905db1a4b393c621a46ea8b0bd1de3fb9022724
HAGRASS2001 <83500189+HAGRASS2001@users.noreply.github.com>
2021-12-16 17:45:28 +0200

    light hack

---
+++ Hack-Runner/Assets/Scripts/hack scripts/light hack/Switch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject up;
+    public GameObject on;
+    public bool ison;
+    public bool isup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        up.SetActive(isup);
+        on.SetActive(ison);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp() // mouse click but in boc colider
+    {
+        isup = !isup;
+        ison = !ison;
+        up.SetActive(isup);
+        on.SetActive(ison);
+        if (ison)
+        {
+            FindObjectOfType<wintext>().switchchange(1);
+        }
+        else {
+            FindObjectOfType<wintext>().switchchange(-1);
+        }
+    }
+}
---
+++ Hack-Runner/Assets/Scripts/hack scripts/light hack/wintext.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wintext : MonoBehaviour
+{
+    public int switchcount;
+    public GameObject WinText;
+    private int oncount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void switchchange(int points) {
+        oncount += points;
+        if (oncount ==  switchcount) {
+            WinText.SetActive(true);
+        }
+    }
+}
