04d7620d9c7896bdff3e0d8a4f7e2a6c133306ae
HAGRASS2001 <83500189+HAGRASS2001@users.noreply.github.com>
2021-12-17 19:34:25 +0200

    animation(walk run idle)

--- Hack-Runner/Assets/Sprites/Player/playercontroller.cs
+++ Hack-Runner/Assets/Sprites/Player/playercontroller.cs
@@ -10,11 +10,13 @@ public class playercontroller : MonoBehaviour
     public KeyCode spacebar;
     public KeyCode leftarrow;
     public KeyCode rightarrow;
+    public KeyCode kick;
     public Transform groundcheck;
     public float groundcheckradius;
     public LayerMask whatisground;
     private bool grounded;
     private Animator anim;
+    private bool kickornot;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +27,12 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(kick))
+        {
+            kickornot = true;
 
+        }
+        anim.SetBool("kickornot", kickornot);
         if (Input.GetKey(spacebar) && grounded)
         {
             jump();
@@ -59,6 +66,7 @@ public class playercontroller : MonoBehaviour
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumphieght);
     }
+
     private void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(groundcheck.position, groundcheckradius, whatisground);
