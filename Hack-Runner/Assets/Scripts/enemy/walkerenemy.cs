f8c4586d0f2bf90b6c584ed114e69fecadb2010d
HAGRASS2001 <83500189+HAGRASS2001@users.noreply.github.com>
2021-12-18 21:49:45 +0200

    scene 3

--- Hack-Runner/Assets/Scripts/enemy/walkerenemy.cs
+++ Hack-Runner/Assets/Scripts/enemy/walkerenemy.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class walkerenemy : enemyController {
+	private Animator anim;
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -28,15 +29,40 @@ public class walkerenemy : enemyController {
 		if (other.tag == "wall")
 		{
 			flip();
-		}
-		else if (other.tag == "enemy")
+		} 
+		if (other.tag == "enemy")
 		{
 			flip();
 		}
 		if (other.tag == "Player")
 		{
+			attack();
 			FindObjectOfType<playerstats>().takedamage(damage);
 			flip();
+		}
+	}
+	void attack()
+	{
+			anim.SetBool("attack", true);
+			StartCoroutine(WaitForHalfASecond());
+	}
+	IEnumerator WaitForHalfASecond()
+	{
+		yield return new WaitForSeconds(.25F);
+		anim.SetBool("attack", false);
+	}
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			if (other.GetContact(0).point.x < this.transform.position.x)
+			{
+				Destroy(this.gameObject, 0f);
+			}
+			else if (other.GetContact(0).point.y > this.transform.position.y)
+			{
+				Destroy(this.gameObject, 0f);
+			}
 		}
 	}
 }
