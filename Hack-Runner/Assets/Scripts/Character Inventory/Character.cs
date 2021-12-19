a592ed411c24ff72f1f27f31229e914f6029fce7
AhmedDesouki <ahmed195141@bue.edu.eg>
2021-12-19 20:00:13 +0200

    Character Inventory without design (Desouki)

---
+++ Hack-Runner/Assets/Scripts/Character Inventory/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Character 
+{
+
+    public string characterName;
+    public Sprite characterSprite;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+}
