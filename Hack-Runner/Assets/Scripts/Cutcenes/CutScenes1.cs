97bbd5567000e1db3dc32bda64d46ad2c917c04d
MahmoudAssem55 <mahmoudassem555666@gmail.com>
2021-12-27 02:30:06 +0200

    assem bg music

--- Hack-Runner/Assets/Scripts/Cutcenes/CutScenes1.cs
+++ Hack-Runner/Assets/Scripts/Cutcenes/CutScenes1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class CutScenes1 : MonoBehaviour
+public class CutScenes1  : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
