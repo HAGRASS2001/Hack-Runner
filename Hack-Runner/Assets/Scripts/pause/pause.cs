35e5757cf3e24b3d5686ddf58b6b12d1f018e0a5
AhmedDesouki <ahmed195141@bue.edu.eg>
2021-12-29 01:29:21 +0200

    Pause button DESOUKII

---
+++ Hack-Runner/Assets/Scripts/pause/pause.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    public GameObject pausescreen;
+    public static bool paused;
+    //public KeyCode pausebutton;
+    void Start()
+    {
+        paused = false;
+        pausescreen.SetActive(false);
+    }
+
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    PauseGame();
+        //}
+        if (Input.GetKeyDown(KeyCode.Escape) && !paused)
+        {
+            PauseGame();
+        }
+        //else if (Input.GetKeyDown(KeyCode.Escape) && !paused)
+        //{
+        //    ResumeGame();
+        //}
+
+    }
+
+        public void PauseGame()
+        {
+        Cursor.lockState = CursorLockMode.None;
+        pausescreen.SetActive(true);
+        paused = true;
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pausescreen.SetActive(false);
+        paused = false;
+        Time.timeScale = 1;
+    }
+    
+    }
+
