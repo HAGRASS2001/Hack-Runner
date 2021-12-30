734557d4299a4e1f023250a86eeca01e32a4a346
HAGRASS2001 <83500189+HAGRASS2001@users.noreply.github.com>
2021-12-30 14:59:28 +0200

    sprites delete

--- Hack-Runner/Assets/Scripts/pause/pause.cs
+++ Hack-Runner/Assets/Scripts/pause/pause.cs
@@ -6,44 +6,40 @@ public class pause : MonoBehaviour
 {
     public GameObject pausescreen;
     public static bool paused;
+
     //public KeyCode pausebutton;
     void Start()
     {
         paused = false;
         pausescreen.SetActive(false);
     }
-
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    PauseGame();
-        //}
-        if (Input.GetKeyDown(KeyCode.Escape) && !paused)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseGame();
+            if (paused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
         }
-        //else if (Input.GetKeyDown(KeyCode.Escape) && !paused)
-        //{
-        //    ResumeGame();
-        //}
 
     }
-
-        public void PauseGame()
-        {
-        Cursor.lockState = CursorLockMode.None;
+    public void PauseGame()
+    {
         pausescreen.SetActive(true);
         paused = true;
         Time.timeScale = 0f;
     }
     public void ResumeGame()
     {
-        Cursor.lockState = CursorLockMode.Locked;
         pausescreen.SetActive(false);
         paused = false;
         Time.timeScale = 1;
     }
     
-    }
+}
 
